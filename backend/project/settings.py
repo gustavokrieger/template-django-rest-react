734c9753070d125bfd2a4dcb9641259b0c932f80
gustavo <gustavopcassol@gmail.com>
2021-03-05 17:42:02 -0300

    added other settings to env variables

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=$johp7ota=qxcs!olr77y*ez4k!h#^#7(t!d5gqx@3c*lb9o&'
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.ALLOWED_HOSTS
 
 
 # Application definition
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': env.POSTGRES_DB,
         'USER': env.POSTGRES_USER,
         'PASSWORD': env.POSTGRES_PASSWORD,
-        'HOST': 'db',
+        'HOST': env.DATABASE_HOST,
         'PORT': 5432,
     }
 }
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = env.TIME_ZONE
 
 USE_I18N = True
 
