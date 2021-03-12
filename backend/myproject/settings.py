ec3c681ba6851087ab6b2f74cfe031cd011a8b24
gustavo <gustavopcassol@gmail.com>
2021-03-12 16:45:10 -0300

    correction on INSTALLED_APPS

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'myapp.apps.AppConfig'
+    'myapp.apps.MyappConfig'
 ]
 
 MIDDLEWARE = [
