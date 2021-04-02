3555908c634ad6448e28dd2de97f1fb6174b0a85
Gustavo Krieger <gustavopcassol@gmail.com>
2021-04-02 12:47:05 -0300

    added static path for development

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -10,8 +12,9 @@ router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+# The static function works only in debug mode.
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
