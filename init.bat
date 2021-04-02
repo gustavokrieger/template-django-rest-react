2acf7d83d3aa74955dfe7e78a5691e61546cf1cc
Gustavo Krieger <gustavopcassol@gmail.com>
2021-04-02 13:03:22 -0300

    corrections and enhancements to files in project root

--- init.bat
+++ init.bat
@@ -1,10 +1,21 @@
-@echo off
+@ECHO OFF
 
-rem  If backend container was created with a different name, change this.
-set container=template-django-rest-react_backend_1
-set manage=docker exec %container% python manage.py
+REM If backend container was created with a different name, change this.
+SET container=template-django-rest-react_backend_1
 
-echo Starting migrate...
+REM Commands
+SET exec=docker exec %container%
+SET manage=%exec% python manage.py
+
+REM Django
+ECHO Starting migrate...
 %manage% migrate
-echo Starting loaddata users...
-%manage% loaddata users
+ECHO Collecting static files...
+%manage% collectstatic --no-input
+
+REM Nginx
+FOR /f "tokens=*" %%a in ('%exec% /etc/init.d/nginx status') do SET output=%%a
+IF "%output%"=="nginx is not running ... failed!" (
+    ECHO Starting nginx...
+    %exec% /usr/sbin/nginx
+)
--- init.sh
+++ init.sh
@@ -2,9 +2,21 @@
 
 # If backend container was created with a different name, change this.
 container=template-django-rest-react_backend_1
-manage="docker exec ${container} python manage.py"
 
+# Commands
+exec="docker exec ${container}"
+manage="${exec} python manage.py"
+
+# Django
 echo Starting migrate...
 $manage migrate
-echo Starting loaddata users...
-$manage loaddata users
+echo Collecting static files...
+$manage collectstatic --no-input
+
+# Nginx
+output=$($exec /etc/init.d/nginx status)
+if [ "$output" = "nginx is not running ... failed!" ]
+then
+    echo Starting nginx...
+    $exec /usr/sbin/nginx
+fi
